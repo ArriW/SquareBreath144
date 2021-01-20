dddd1ae4c1ec02a4d635662d623a3a3886d52847
KidConj <arringtonwalters@gmail.com>
2021-01-19 19:01:26 -0800

    delayed start

--- SquarBreath144_snake/SquarBreath144_snake.ino
+++ SquarBreath144_snake/SquarBreath144_snake.ino
@@ -9,23 +9,39 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 // Parameters
 int whiteLength         = 5;
 float stripLength        =123;
-
 float maxbrightness     =175;
 float minbrightness = 75;
 int timer =16; // seconds
 float brightnessStep  =maxbrightness*(minbrightness/maxbrightness)*
   (stripLength/strip.numPixels())/timer*2; 
 
+// variables for starting main loop
+int counter=0;
+
+
 void intro(){
 int period=1000;
-for (unsigned long elapsedMillis=millis(),  previousMillis = millis(), introbrightness,dampen=1;
-             (elapsedMillis-previousMillis) <3*period;
-           elapsedMillis=millis()){
-
-            introbrightness=minbrightness+abs(minbrightness*sin(PI*(elapsedMillis-previousMillis)/(period)));
-            strip.fill(strip.Color(strip.gamma8(introbrightness), 0, 0, strip.gamma8(introbrightness)));
-            strip.show();
+for (unsigned long elapsedMillis=millis(),  previousMillis = millis(),
+  introbrightness,dampen=1;elapsedMillis-previousMillis <3*period;
+  elapsedMillis=millis()){
+  
+  introbrightness=minbrightness+abs(minbrightness*sin(PI*(elapsedMillis-previousMillis)/(period)));
+  strip.fill(strip.Color(strip.gamma8(introbrightness), 0, 0, strip.gamma8(introbrightness)));
+  strip.show();
             }
+}
+
+void setgo(){
+  for(int i=0; i <2;i++){
+    strip.setPixelColor(1, strip.Color(strip.gamma8(maxbrightness), 0, 0, strip.gamma8(maxbrightness)));
+    strip.setPixelColor(2, strip.Color(strip.gamma8(maxbrightness), 0, 0, strip.gamma8(maxbrightness)));
+    strip.show();  
+    delay(1000);
+    strip.setPixelColor(1, strip.Color(0, 0, 0, 0));
+    strip.setPixelColor(2, strip.Color(0, 0, 0, 0));
+    strip.show(); 
+    delay(1000); 
+  }
 }
 
 void Square() {
@@ -65,9 +81,7 @@ int      x              = 0;
         strip.setPixelColor(x = i - whiteLength, strip.Color(strip.gamma8(j), 0, 0, strip.gamma8(j)));
         strip.show();
         }
-
       }
-//      markers();
     }
 
            loopend=millis();
@@ -85,36 +99,32 @@ void setup(){
   strip.setBrightness(BRIGHTNESS);
 
   pinMode (IRSensor, INPUT); // sensor pin INPUT
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-  strip.fill(strip.Color(strip.gamma8(minbrightness), 0, 0, strip.gamma8(minbrightness)));
-   strip.show();  
-   
+
+   Serial.println(counter);
   int statusSensor = digitalRead (IRSensor);
-  
-  if (statusSensor == 1)
-  {
+  strip.fill(strip.Color(strip.gamma8(minbrightness), 0, 0, strip.gamma8(minbrightness)));
+  strip.show();  
+
+//Build threshold for starting
+  if (statusSensor == 1){
+    counter++;
+    }
+  else{
+    (counter=counter-1);
+    if(counter<0){counter=0;}
+  }
+  if (counter>5){
     intro();
-    for(int i=0; i <2;i++){
-          strip.setPixelColor(1, strip.Color(strip.gamma8(maxbrightness), 0, 0, strip.gamma8(maxbrightness)));
-          strip.setPixelColor(2, strip.Color(strip.gamma8(maxbrightness), 0, 0, strip.gamma8(maxbrightness)));
-             strip.show();  
-          delay(1000);
-          strip.setPixelColor(1, strip.Color(0, 0, 0, 0));
-          strip.setPixelColor(2, strip.Color(0, 0, 0, 0));
-             strip.show(); 
-          delay(1000); 
-      }
+    setgo();
     Square();
     intro();
   }
-  else
-  { 
-  }
-//Change ideal color if used a certain amount during the day reducing it to nothing if not used within 24hrs
-//Create a threshold for the beginning sequence to start
-// Move starting position to left side
-
+delay(1000);  
 }
+//Change idle color if used a certain amount during the day reducing it to nothing if not used within 24hrs
+//Create contiuation if 
+// Move starting position to left side
